15997b1811a176e6230fe5db66f7f4feaa62c68f
dsokolov <dsokolov060388@gmail.com>
2023-09-21 06:53:05 +0300

    feat: Добавлены первые файлы

--- src/base.py
+++ src/base.py
@@ -1,3 +1,9 @@
-#
+import undetected_chromedriver as uc
+import time
 
-###
+
+driver = uc.Chrome(headless=False)
+url = "https://travel.mts.ru/"
+driver.get(url)
+time.sleep(15)
+driver.quit()
---
+++ src/pages/main_page/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPage:
+
+    MAIN = (By.XPATH, '//body[@class="antialiased"]')
