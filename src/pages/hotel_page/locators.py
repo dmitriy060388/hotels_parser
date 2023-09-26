bf5d80f12afbb190f72b5eda2015eb31d35659ff
dsokolov <dsokolov060388@gmail.com>
2023-09-26 12:18:05 +0300

    fix: Добавления не отправленные в коммите - 584a2624817116bbb609c3b1353efe7ea1b4da33

--- src/pages/hotel_page/locators.py
+++ src/pages/hotel_page/locators.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class HotelPageLocators:
 
-    CHOOSE_HOTEL = (By.XPATH, '//button[@data-test='
+    CHOOSE_ROOMS = (By.XPATH, '//button[@data-test='
                     '"select_rooms"]')
     HOTEL_NOT_FOUND = (
         By.XPATH, '//h2[@class="p2-medium"][contains(text(), "По вашему запросу ничего не нашлось")]'
--- src/pages/hotel_page/page.py
+++ src/pages/hotel_page/page.py
@@ -9,9 +9,9 @@ class HotelPage(BaseClass):
 
         # Проверка на наличие предложений с отелями
         try:
-            self._wait_visible_element(HotelPageLocators.CHOOSE_HOTEL)
+            self._wait_visible_element(HotelPageLocators.CHOOSE_ROOMS)
         except TimeoutException:
             error = self._find_element(HotelPageLocators.HOTEL_NOT_FOUND).text
             raise TimeoutException("Ошибка - {}".format(error))
 
-        self._click_visible_element(HotelPageLocators.CHOOSE_HOTEL)
+        self._click_visible_element(HotelPageLocators.CHOOSE_ROOMS)
