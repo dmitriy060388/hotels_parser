888afa46b7afbb8168c885abb39088344e48c144
hineni92 <eswerdelicht83@gmail.com>
2023-10-01 02:24:04 +0300

    расширен комментарий

--- src/psql_connector/psql.py
+++ src/psql_connector/psql.py
@@ -8,8 +8,9 @@ except:
     # вывод ошибки при невозможности подключения к БД
     print('Can`t establish connection to database')
 
-# работа с БД, предполагается, что уже создан экземпляр mts_test_1 с доступом от учетки mtsadmin
+# работа с БД, предполагается, что уже создана схема mts_test_1 внутри бд mts_test_1 с доступом от учетки mtsadmin
 cursor = conn.cursor()
 cursor.execute("CREATE TABLE mts_test_1.results (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, hotel_name VARCHAR(200), date DATE, price INT, category VARCHAR(128), breakfast BOOLEAN);")
+cursor.fetchall()
 print('yeah2')
 conn.close()
