fab324e371e9e8f05658ad7fc78f866a434ab4f6
hineni92 <eswerdelicht83@gmail.com>
2023-10-02 22:28:31 +0300

    обновил тестовый файл для postgres

--- src/psql_connector/postgres_test.py
+++ src/psql_connector/postgres_test.py
@@ -3,9 +3,11 @@ import psycopg2
 
 try:
     # подключение к БД
-    conn = psycopg2.connect(dbname='mts_test_1', user='postgres', password='P@ssw0rd!', host='localhost', port='5432')
+    conn = psycopg2.connect(dbname='mts_test_1', user='admcosmos', password='F9ut@R*_22M', host='localhost', port='5432')
     conn.autocommit = True
-    print('yeah1')
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE mts_test_1.results (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, hotel_name VARCHAR(200), date DATE, price INT, category VARCHAR(128), breakfast BOOLEAN);")
+    cursor.fetchall()
 except:
     # вывод ошибки при невозможности подключения к БД
     print('Can`t establish connection to database')
