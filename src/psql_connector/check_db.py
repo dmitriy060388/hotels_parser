6a3aecf87e92e93b542ec22a89eed54637f3b236
hineni92 <eswerdelicht83@gmail.com>
2023-10-04 15:18:17 +0300

    передача всех атрибутов в переменных для БД

--- src/psql_connector/check_db.py
+++ src/psql_connector/check_db.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 
 conn = psycopg2.connect(dbname='mts_test_1', user='postgres', password='P@ssw0rd!', host='localhost', port='5432')
 conn.autocommit = True
@@ -9,16 +10,19 @@ conn.autocommit = True
 #def psinsert(dbcon, id, hotel_name, date, price, category, breakfast)
 
 
-def check_existence(dbcon, table_str):
+def check_existence(dbcon, scheme_name, table_str):
 #    table_str = "results"
     exists = False
     try:
         cur = dbcon.cursor()
-        psqlexec = "select exists(select * from mts_test1.mts_scheme where table_name= %s)"
-        psqldata = (table_str,)
+        cur.execute(
+            sql.SQL("select exists (select * from {} where table_name=%s)")
+            .format(sql.Identifier(scheme_name), (table_str)))
+#1        psqlexec = "select exists(select * from mts_test1.mts_scheme where table_name= %s)"
+#1        psqldata = (table_str,)
 #       cur.execute("select exists(select * from mts_scheme where table_name=%s)", (table_str,))
-#        cur.execute("select exists(select * from %s where table_name= %s)", (schema_name, table_str,))
-        cur.execute(psqlexec, psqldata)
+#2        cur.execute("select exists(select * from %s where table_name= %s)", (scheme_name, table_str,))
+#1        cur.execute(psqlexec, psqldata)
         cur.fetchone()[0]
         print ("exists")
         cur.close()
@@ -27,5 +31,5 @@ def check_existence(dbcon, table_str):
         print (e)
     return exists
 
-check_existence(conn, "result")
+check_existence(conn, "mts_test1.mts_scheme", "result")
 #def table_exists(conn, "results")
